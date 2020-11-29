3988876f13580795bbe7d8e9e23d2858e7cdc81d
Aya-Mahmoud-99 <ayamahmoudabdelfatah99@gmail.com>
2020-11-29 14:56:31 +0200

    Modification to entity

--- source/common/ECS/Entity.cpp
+++ source/common/ECS/Entity.cpp
@@ -20,7 +20,7 @@ for (auto i : components) {
 if ((t = dynamic_cast<T*>(i)) != nullptr) return t;
 }
 
-return nullptr;
+return NULL;
 }
 Entity::~Entity() {
     for(int i=0;i<components.size();i++){
